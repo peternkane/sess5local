c962af0709f10074077edf66d6de2363287b6dd6
peternkane <peternkane24@gmail.com>
2021-05-28 16:04:00 +0300

    updated

--- Program.cs
+++ Program.cs
@@ -8,8 +8,12 @@ namespace sess5local
            static int spacetravelorAGE = 631154000;
            static int earthseconds = 31557600;
 
-           static float amount1;
-           static float amount3;
+           static float earthaage;
+           static float Venusaage;
+           static float marsaage;
+           static float jupiteraage;
+           static float neptuneaage;
+        //    static float amount3;
           
             //Planet names and their orbital periods
            static string[] planetnames = new string[4];
@@ -31,7 +35,12 @@ namespace sess5local
             orbitalyears[3] = 164.79132f;
 
             Console.WriteLine("Space traveller is " + Program.earthage(spacetravelorAGE, earthseconds) + " years old");
-            Console.WriteLine( Program.otherplanetsage(amount1));
+
+             Console.WriteLine(planetnames[0] + Program.venusage(earthaage) + " Earth-years old");
+             Console.WriteLine(planetnames[1] + Program.marsage(earthaage) + " Earth-years old");
+             Console.WriteLine(planetnames[2] + Program.jupiterage(earthaage) + " Earth-years old");
+             Console.WriteLine(planetnames[3] + Program.neptuneage(earthaage) + " Earth-years old");
+            // Console.WriteLine( Program.otherplanetsage(amount1));
         }
 
         public static float earthage (int spacetravelorAGE, int earthseconds){
@@ -39,26 +48,48 @@ namespace sess5local
             float ageinyears = spacetravelorAGE/earthseconds;
 
             // round to two decimal places.
-            amount1 = (float)Math.Round(ageinyears, 2);
+            earthaage = (float)Math.Round(ageinyears, 2);
             //display the results.
-            return amount1;
+            return earthaage;
 
         } 
-        public static  string otherplanetsage(float amount1){
+        // public static  string otherplanetsage(float amount1){
 
-             for(int i = 0;  i < planetnames.Length; i++ ){  
+        //      for(int i = 0;  i < planetnames.Length; i++ ){  
                
-                        float Amount2 = ( Program.earthage(spacetravelorAGE, earthseconds) / orbitalyears[i]);
-                        // round to two decimal places.
-                        amount3 = MathF.Round(Amount2, 2);
-                        // display the results.
-                        Console.WriteLine(planetnames[i] + " " + amount3 + " Earth-years old");                        
+        //                 float Amount2 = ( Program.earthage(spacetravelorAGE, earthseconds) / orbitalyears[i]);
+        //                 // round to two decimal places.
+        //                 amount3 = MathF.Round(Amount2, 2);
+        //                 // display the results.
+        //                 Console.WriteLine(planetnames[i] + " " + amount3 + " Earth-years old");                        
                        
-            } 
+        //     } 
 
-           return "";
+        //    return "";
 
+        // }
+
+        public static float venusage(float earthaage){
+            float venusage = Program.earthage(spacetravelorAGE, earthseconds) /  orbitalyears[0];
+            Venusaage = MathF.Round(venusage, 2);
+            return Venusaage;
         }
+        public static float marsage(float earthaage){
+            float marsage = Program.earthage(spacetravelorAGE, earthseconds) /  orbitalyears[1];
+            marsaage = MathF.Round(marsage, 2);
+            return marsaage;
+        }
+        public static float jupiterage(float earthaage){
+            float jupiterage = Program.earthage(spacetravelorAGE, earthseconds) /  orbitalyears[2];
+            jupiteraage = MathF.Round(jupiterage, 2);
+            return jupiteraage;
+        }
+        public static float neptuneage(float earthaage){
+            float neptuneage = Program.earthage(spacetravelorAGE, earthseconds) /  orbitalyears[3];
+            neptuneaage = MathF.Round(neptuneage, 2);
+            return neptuneaage;
+        }
+
 
         
     }
